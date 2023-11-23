5ea9d19a87b0afeaffd2aade52acd5a0e1ec3d72
Jean-Gabriel Hanania <83063812+Jegahan@users.noreply.github.com>
2023-11-23 16:04:45 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -214,7 +214,15 @@ async function writeFile() {
   window.addEventListener('storage', onStorageChanged);
   }
 
+  /* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
 
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+} 
   
 }
 
