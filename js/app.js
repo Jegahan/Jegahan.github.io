a25f85348cee46f4479921cac73ce1998f1682f5
Jean-Gabriel Hanania <83063812+Jegahan@users.noreply.github.com>
2023-11-23 16:01:41 +0000

    test

--- js/app.js
+++ js/app.js
@@ -3,27 +3,12 @@ const container = document.querySelector(".container")
 var target = document.getElementById('target');
 var watchId;
 
-function appendLocation(location, verb) {
-  verb = verb || 'updated';
-  var newLocation = document.createElement('p');
-  newLocation.innerHTML = 'Location ' + verb + ': ' + location.coords.latitude + ', ' + location.coords.longitude + '';
-  target.appendChild(newLocation);
-}
 
-if ('geolocation' in navigator) {
-  document.getElementById('askButton').addEventListener('click', function () {
-    navigator.geolocation.getCurrentPosition(function (location) {
-      appendLocation(location, 'fetched');
-    });
-    watchId = navigator.geolocation.watchPosition(appendLocation);
-  });
-} else {
-  target.innerText = 'Geolocation API not supported.';
-}
+
 
 if ('LinearAccelerationSensor' in window && 'Gyroscope' in window) {
   document.getElementById('moApi').innerHTML = 'Generic Sensor API';
-  
+
   let lastReadingTimestamp;
   let accelerometer = new LinearAccelerationSensor();
   accelerometer.addEventListener('reading', e => {
@@ -34,13 +19,13 @@ if ('LinearAccelerationSensor' in window && 'Gyroscope' in window) {
     accelerationHandler(accelerometer, 'moAccel');
   });
   accelerometer.start();
-  
+
   if ('GravitySensor' in window) {
     let gravity = new GravitySensor();
     gravity.addEventListener('reading', e => accelerationHandler(gravity, 'moAccelGrav'));
     gravity.start();
   }
-  
+
   let gyroscope = new Gyroscope();
   gyroscope.addEventListener('reading', e => rotationHandler({
     alpha: gyroscope.x,
@@ -48,17 +33,17 @@ if ('LinearAccelerationSensor' in window && 'Gyroscope' in window) {
     gamma: gyroscope.z
   }));
   gyroscope.start();
-  
+
 } else if ('DeviceMotionEvent' in window) {
   document.getElementById('moApi').innerHTML = 'Device Motion API';
-  
+
   var onDeviceMotion = function (eventData) {
     accelerationHandler(eventData.acceleration, 'moAccel');
     accelerationHandler(eventData.accelerationIncludingGravity, 'moAccelGrav');
     rotationHandler(eventData.rotationRate);
     intervalHandler(eventData.interval);
   }
-  
+
   window.addEventListener('devicemotion', onDeviceMotion, false);
 } else {
   document.getElementById('moApi').innerHTML = 'No Accelerometer & Gyroscope API available';
@@ -86,135 +71,7 @@ function intervalHandler(interval) {
   document.getElementById("moInterval").innerHTML = interval;
 }
 
-
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", function () {
-        navigator.serviceWorker
-            .register("/serviceWorker.js")
-            .then(res => console.log("service worker registered"))
-            .catch(err => console.log("service worker not registered", err))
-    })
-  if ('storage' in navigator && 'estimate' in navigator.storage) {
-  navigator.storage.estimate()
-    .then(estimate => {
-      document.getElementById('usage').innerHTML = estimate.usage;
-      document.getElementById('quota').innerHTML = estimate.quota;
-      document.getElementById('percent').innerHTML = (estimate.usage * 100 / estimate.quota).toFixed(0);
-    });
-}
-
-if ('storage' in navigator && 'persisted' in navigator.storage) {
-  navigator.storage.persisted()
-    .then(persisted => {
-      document.getElementById('persisted').innerHTML = persisted ? 'persisted' : 'not persisted';
-    });
-}
-
-function requestPersistence() {
-  if ('storage' in navigator && 'persist' in navigator.storage) {
-    navigator.storage.persist()
-      .then(persisted => {
-        document.getElementById('persisted').innerHTML = persisted ? 'persisted' : 'not persisted';
-      });
-  }
-}
-  function getReadFile(reader, i) {
-  return function () {
-    var li = document.querySelector('[data-idx="' + i + '"]');
-
-    li.innerHTML += 'File starts with "' + reader.result.substr(0, 25) + '"';
-  }
-}
-
-function readFiles(files) {
-  document.getElementById('count').innerHTML = files.length;
-
-  var target = document.getElementById('target');
-  target.innerHTML = '';
-
-  for (var i = 0; i < files.length; ++i) {
-    var item = document.createElement('li');
-    item.setAttribute('data-idx', i);
-    var file = files[i];
-
-    var reader = new FileReader();
-    reader.addEventListener('load', getReadFile(reader, i));
-    reader.readAsText(file);
-
-    item.innerHTML = '' + file.name + ', ' + file.type + ', ' + file.size + ' bytes, last modified ' + file.lastModifiedDate + '';
-    target.appendChild(item);
-  };
-}
-
-async function writeFile() {
-  if (!window.chooseFileSystemEntries) {
-    alert('Native File System API not supported');
-    return;
-  }
-  
-  const target = document.getElementById('target');
-  target.innerHTML = 'Opening file handle...';
-  
-  const handle = await window.chooseFileSystemEntries({
-    type: 'save-file',
-  });
-  
-  const file = await handle.getFile()
-  const writer = await handle.createWriter();
-  await writer.write(0, 'Hello world from What Web Can Do!');
-  await writer.close()
-  
-  target.innerHTML = 'Test content written to ' + file.name + '.';
-}
-
-  if ('localStorage' in window || 'sessionStorage' in window) {
-  var selectedEngine;
-
-  var logTarget = document.getElementById('target');
-  var valueInput = document.getElementById('value');
-
-  var reloadInputValue = function () {
-  console.log(selectedEngine, window[selectedEngine].getItem('myKey'))
-    valueInput.value = window[selectedEngine].getItem('myKey') || '';
-  }
-  
-  var selectEngine = function (engine) {
-    selectedEngine = engine;
-    reloadInputValue();
-  };
-
-  function handleChange(change) {
-    var timeBadge = new Date().toTimeString().split(' ')[0];
-    var newState = document.createElement('p');
-    newState.innerHTML = '' + timeBadge + ' ' + change + '.';
-    logTarget.appendChild(newState);
-  }
-
-  var radios = document.querySelectorAll('#selectEngine input');
-  for (var i = 0; i < radios.length; ++i) {
-    radios[i].addEventListener('change', function () {
-      selectEngine(this.value)
-    });
-  }
-  
-  selectEngine('localStorage');
-
-  valueInput.addEventListener('keyup', function () {
-    window[selectedEngine].setItem('myKey', this.value);
-  });
-
-  var onStorageChanged = function (change) {
-    var engine = change.storageArea === window.localStorage ? 'localStorage' : 'sessionStorage';
-    handleChange('External change in ' + engine + ': key ' + change.key + ' changed from ' + change.oldValue + ' to ' + change.newValue + '');
-    if (engine === selectedEngine) {
-      reloadInputValue();
-    }
-  }
-
-  window.addEventListener('storage', onStorageChanged);
-  }
-
-  /* Set the width of the side navigation to 250px */
+/* Set the width of the side navigation to 250px */
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
 }
@@ -222,7 +79,7 @@ function openNav() {
 /* Set the width of the side navigation to 0 */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
-} 
+}
 
 
 
@@ -235,7 +92,7 @@ if ('getBattery' in navigator || ('battery' in navigator && 'Promise' in window)
     newState.innerHTML = '' + timeBadge + ' ' + change + '.';
     target.appendChild(newState);
   }
-  
+
   function onChargingChange() {
     handleChange('Battery charging changed to ' + (this.charging ? 'charging' : 'discharging') + '')
   }
@@ -250,25 +107,152 @@ if ('getBattery' in navigator || ('battery' in navigator && 'Promise' in window)
   }
 
   var batteryPromise;
-  
+
   if ('getBattery' in navigator) {
     batteryPromise = navigator.getBattery();
   } else {
     batteryPromise = Promise.resolve(navigator.battery);
   }
-  
+
   batteryPromise.then(function (battery) {
     document.getElementById('charging').innerHTML = battery.charging ? 'charging' : 'discharging';
     document.getElementById('chargingTime').innerHTML = battery.chargingTime + ' s';
     document.getElementById('dischargingTime').innerHTML = battery.dischargingTime + ' s';
     document.getElementById('level').innerHTML = battery.level;
-    
+
     battery.addEventListener('chargingchange', onChargingChange);
     battery.addEventListener('chargingtimechange', onChargingTimeChange);
     battery.addEventListener('dischargingtimechange', onDischargingTimeChange);
     battery.addEventListener('levelchange', onLevelChange);
   });
 }
-  
+
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err))
+  })
+  if ('storage' in navigator && 'estimate' in navigator.storage) {
+    navigator.storage.estimate()
+      .then(estimate => {
+        document.getElementById('usage').innerHTML = estimate.usage;
+        document.getElementById('quota').innerHTML = estimate.quota;
+        document.getElementById('percent').innerHTML = (estimate.usage * 100 / estimate.quota).toFixed(0);
+      });
+  }
+
+  if ('storage' in navigator && 'persisted' in navigator.storage) {
+    navigator.storage.persisted()
+      .then(persisted => {
+        document.getElementById('persisted').innerHTML = persisted ? 'persisted' : 'not persisted';
+      });
+  }
+
+  function requestPersistence() {
+    if ('storage' in navigator && 'persist' in navigator.storage) {
+      navigator.storage.persist()
+        .then(persisted => {
+          document.getElementById('persisted').innerHTML = persisted ? 'persisted' : 'not persisted';
+        });
+    }
+  }
+  function getReadFile(reader, i) {
+    return function () {
+      var li = document.querySelector('[data-idx="' + i + '"]');
+
+      li.innerHTML += 'File starts with "' + reader.result.substr(0, 25) + '"';
+    }
+  }
+
+  function readFiles(files) {
+    document.getElementById('count').innerHTML = files.length;
+
+    var target = document.getElementById('target');
+    target.innerHTML = '';
+
+    for (var i = 0; i < files.length; ++i) {
+      var item = document.createElement('li');
+      item.setAttribute('data-idx', i);
+      var file = files[i];
+
+      var reader = new FileReader();
+      reader.addEventListener('load', getReadFile(reader, i));
+      reader.readAsText(file);
+
+      item.innerHTML = '' + file.name + ', ' + file.type + ', ' + file.size + ' bytes, last modified ' + file.lastModifiedDate + '';
+      target.appendChild(item);
+    };
+  }
+
+  async function writeFile() {
+    if (!window.chooseFileSystemEntries) {
+      alert('Native File System API not supported');
+      return;
+    }
+
+    const target = document.getElementById('target');
+    target.innerHTML = 'Opening file handle...';
+
+    const handle = await window.chooseFileSystemEntries({
+      type: 'save-file',
+    });
+
+    const file = await handle.getFile()
+    const writer = await handle.createWriter();
+    await writer.write(0, 'Hello world from What Web Can Do!');
+    await writer.close()
+
+    target.innerHTML = 'Test content written to ' + file.name + '.';
+  }
+
+  if ('localStorage' in window || 'sessionStorage' in window) {
+    var selectedEngine;
+
+    var logTarget = document.getElementById('target');
+    var valueInput = document.getElementById('value');
+
+    var reloadInputValue = function () {
+      console.log(selectedEngine, window[selectedEngine].getItem('myKey'))
+      valueInput.value = window[selectedEngine].getItem('myKey') || '';
+    }
+
+    var selectEngine = function (engine) {
+      selectedEngine = engine;
+      reloadInputValue();
+    };
+
+    function handleChange(change) {
+      var timeBadge = new Date().toTimeString().split(' ')[0];
+      var newState = document.createElement('p');
+      newState.innerHTML = '' + timeBadge + ' ' + change + '.';
+      logTarget.appendChild(newState);
+    }
+
+    var radios = document.querySelectorAll('#selectEngine input');
+    for (var i = 0; i < radios.length; ++i) {
+      radios[i].addEventListener('change', function () {
+        selectEngine(this.value)
+      });
+    }
+
+    selectEngine('localStorage');
+
+    valueInput.addEventListener('keyup', function () {
+      window[selectedEngine].setItem('myKey', this.value);
+    });
+
+    var onStorageChanged = function (change) {
+      var engine = change.storageArea === window.localStorage ? 'localStorage' : 'sessionStorage';
+      handleChange('External change in ' + engine + ': key ' + change.key + ' changed from ' + change.oldValue + ' to ' + change.newValue + '');
+      if (engine === selectedEngine) {
+        reloadInputValue();
+      }
+    }
+
+    window.addEventListener('storage', onStorageChanged);
+  }
 }
 
