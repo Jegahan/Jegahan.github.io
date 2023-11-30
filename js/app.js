0559d3e1889d94eaada4027b420b4d4535d8d116
Jean-Gabriel Hanania <83063812+Jegahan@users.noreply.github.com>
2023-11-30 14:21:19 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -92,7 +92,7 @@ if ('getBattery' in navigator || ('battery' in navigator && 'Promise' in window)
     newState.innerHTML = '' + timeBadge + ' ' + change + '.';
     target.appendChild(newState);
   }
-
+  
   function onChargingChange() {
     handleChange('Battery charging changed to ' + (this.charging ? 'charging' : 'discharging') + '')
   }
@@ -107,19 +107,19 @@ if ('getBattery' in navigator || ('battery' in navigator && 'Promise' in window)
   }
 
   var batteryPromise;
-
+  
   if ('getBattery' in navigator) {
     batteryPromise = navigator.getBattery();
   } else {
     batteryPromise = Promise.resolve(navigator.battery);
   }
-
+  
   batteryPromise.then(function (battery) {
     document.getElementById('charging').innerHTML = battery.charging ? 'charging' : 'discharging';
     document.getElementById('chargingTime').innerHTML = battery.chargingTime + ' s';
     document.getElementById('dischargingTime').innerHTML = battery.dischargingTime + ' s';
     document.getElementById('level').innerHTML = battery.level;
-
+    
     battery.addEventListener('chargingchange', onChargingChange);
     battery.addEventListener('chargingtimechange', onChargingTimeChange);
     battery.addEventListener('dischargingtimechange', onDischargingTimeChange);
