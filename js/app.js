7eea329b8a6e3ca650f56f7400e3071e77ae21ae
Jean-Gabriel Hanania <83063812+Jegahan@users.noreply.github.com>
2023-11-30 16:12:47 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,125 +1,69 @@
-/**Battery Status**/
-if ('getBattery' in navigator || ('battery' in navigator && 'Promise' in window)) {
-  var target = document.getElementById('target');
+// app.js
+document.addEventListener('DOMContentLoaded', () => {
+  // Check for cached transactions and load them
+  const cachedTransactions = JSON.parse(localStorage.getItem('transactions')) || [];
+  const transactionTableBody = document.getElementById('transactionTableBody');
+  const currentBalanceSpan = document.getElementById('currentBalance');
 
-  function handleChange(change) {
-    var timeBadge = new Date().toTimeString().split(' ')[0];
-    var newState = document.createElement('p');
-    newState.innerHTML = '' + timeBadge + ' ' + change + '.';
-    target.appendChild(newState);
-  }
-  
-  function onChargingChange() {
-    handleChange('Battery charging changed to ' + (this.charging ? 'charging' : 'discharging') + '')
-  }
-  function onChargingTimeChange() {
-    handleChange('Battery charging time changed to ' + this.chargingTime + ' s');
-  }
-  function onDischargingTimeChange() {
-    handleChange('Battery discharging time changed to ' + this.dischargingTime + ' s');
-  }
-  function onLevelChange() {
-    handleChange('Battery level changed to ' + this.level + '');
-  }
+  let transactions = cachedTransactions;
 
-  var batteryPromise;
-  
-  if ('getBattery' in navigator) {
-    batteryPromise = navigator.getBattery();
-  } else {
-    batteryPromise = Promise.resolve(navigator.battery);
-  }
-  
-  batteryPromise.then(function (battery) {
-    document.getElementById('charging').innerHTML = battery.charging ? 'charging' : 'discharging';
-    document.getElementById('chargingTime').innerHTML = battery.chargingTime + ' s';
-    document.getElementById('dischargingTime').innerHTML = battery.dischargingTime + ' s';
-    document.getElementById('level').innerHTML = battery.level;
-    
-    battery.addEventListener('chargingchange', onChargingChange);
-    battery.addEventListener('chargingtimechange', onChargingTimeChange);
-    battery.addEventListener('dischargingtimechange', onDischargingTimeChange);
-    battery.addEventListener('levelchange', onLevelChange);
-  });
-}
+  // Display transactions from cache
+  displayTransactions();
 
+  // Display current balance
+  updateBalance();
 
-/*Acceleration*/
-if ('LinearAccelerationSensor' in window && 'Gyroscope' in window) {
-  document.getElementById('moApi').innerHTML = 'Generic Sensor API';
+  // Function to add a new transaction
+  window.addTransaction = function () {
+    const transactionInput = document.getElementById('transaction');
+    const amountInput = document.getElementById('amount');
 
-  let lastReadingTimestamp;
-  let accelerometer = new LinearAccelerationSensor();
-  accelerometer.addEventListener('reading', e => {
-    if (lastReadingTimestamp) {
-      intervalHandler(Math.round(accelerometer.timestamp - lastReadingTimestamp));
+    const transaction = transactionInput.value.trim();
+    const amount = parseFloat(amountInput.value);
+
+    if (transaction && !isNaN(amount)) {
+      const newTransaction = {
+        date: new Date().toLocaleDateString(),
+        transaction,
+        amount,
+      };
+
+      transactions.push(newTransaction);
+      saveTransactionsToLocalStorage();
+      displayTransactions();
+      updateBalance();
+
+      // Clear input fields
+      transactionInput.value = '';
+      amountInput.value = '';
+    } else {
+      alert('Please enter valid transaction details.');
     }
-    lastReadingTimestamp = accelerometer.timestamp
-    accelerationHandler(accelerometer, 'moAccel');
-  });
-  accelerometer.start();
+  };
 
-  if ('GravitySensor' in window) {
-    let gravity = new GravitySensor();
-    gravity.addEventListener('reading', e => accelerationHandler(gravity, 'moAccelGrav'));
-    gravity.start();
+  // Function to display transactions in the table
+  function displayTransactions() {
+    transactionTableBody.innerHTML = '';
+
+    transactions.forEach((transaction) => {
+      const row = document.createElement('tr');
+      row.innerHTML = `
+        <td>${transaction.date}</td>
+        <td>${transaction.transaction}</td>
+        <td>${transaction.amount}</td>
+      `;
+      transactionTableBody.appendChild(row);
+    });
   }
 
-  let gyroscope = new Gyroscope();
-  gyroscope.addEventListener('reading', e => rotationHandler({
-    alpha: gyroscope.x,
-    beta: gyroscope.y,
-    gamma: gyroscope.z
-  }));
-  gyroscope.start();
-
-} else if ('DeviceMotionEvent' in window) {
-  document.getElementById('moApi').innerHTML = 'Device Motion API';
-
-  var onDeviceMotion = function (eventData) {
-    accelerationHandler(eventData.acceleration, 'moAccel');
-    accelerationHandler(eventData.accelerationIncludingGravity, 'moAccelGrav');
-    rotationHandler(eventData.rotationRate);
-    intervalHandler(eventData.interval);
+  // Function to update the current balance
+  function updateBalance() {
+    const totalBalance = transactions.reduce((total, transaction) => total + transaction.amount, 0);
+    currentBalanceSpan.textContent = totalBalance.toFixed(2);
   }
 
-  window.addEventListener('devicemotion', onDeviceMotion, false);
-} else {
-  document.getElementById('moApi').innerHTML = 'No Accelerometer & Gyroscope API available';
-}
-
-function accelerationHandler(acceleration, targetId) {
-  var info, xyz = "[X, Y, Z]";
-
-  info = xyz.replace("X", acceleration.x && acceleration.x.toFixed(3));
-  info = info.replace("Y", acceleration.y && acceleration.y.toFixed(3));
-  info = info.replace("Z", acceleration.z && acceleration.z.toFixed(3));
-  document.getElementById(targetId).innerHTML = info;
-}
-
-function rotationHandler(rotation) {
-  var info, xyz = "[X, Y, Z]";
-
-  info = xyz.replace("X", rotation.alpha && rotation.alpha.toFixed(3));
-  info = info.replace("Y", rotation.beta && rotation.beta.toFixed(3));
-  info = info.replace("Z", rotation.gamma && rotation.gamma.toFixed(3));
-  document.getElementById("moRotation").innerHTML = info;
-}
-
-function intervalHandler(interval) {
-  document.getElementById("moInterval").innerHTML = interval;
-}
-
-/* Set the width of the side navigation to 250px */
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-}
-
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-}
-
-
-
-
+  // Function to save transactions to local storage
+  function saveTransactionsToLocalStorage() {
+    localStorage.setItem('transactions', JSON.stringify(transactions));
+  }
+});
