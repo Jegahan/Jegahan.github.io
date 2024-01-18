7ed19161c3092b94303b77d0e17c5db7a2c7a7d4
Jean-Gabriel Hanania <83063812+Jegahan@users.noreply.github.com>
2024-01-18 15:42:35 +0000

    isntkoqnefwpmt

--- js/app.js
+++ js/app.js
@@ -1,60 +1,60 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Check if service workers are supported
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('sw.js')
-            .then(registration => {
-                console.log('Service Worker registered with scope:', registration.scope);
-            })
-            .catch(error => {
-                console.error('Service Worker registration failed:', error);
-            });
+  // Check if service workers are supported
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js')
+      .then(registration => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch(error => {
+        console.error('Service Worker registration failed:', error);
+      });
+  }
+
+  function startDrag(e) {
+    this.ontouchmove = this.onmspointermove = moveDrag;
+
+    this.ontouchend = this.onmspointerup = function () {
+      this.ontouchmove = this.onmspointermove = null;
+      this.ontouchend = this.onmspointerup = null;
     }
 
-    function startDrag(e) {
-      this.ontouchmove = this.onmspointermove = moveDrag;
-    
-      this.ontouchend = this.onmspointerup = function () {
-        this.ontouchmove = this.onmspointermove = null;
-        this.ontouchend = this.onmspointerup = null;
-      }
-    
-      var pos = [this.offsetLeft, this.offsetTop];
-      var that = this;
-      var origin = getCoors(e);
-    
-      function moveDrag(e) {
-        var currentPos = getCoors(e);
-        var deltaX = currentPos[0] - origin[0];
-        var deltaY = currentPos[1] - origin[1];
-        this.style.left = (pos[0] + deltaX) + 'px';
-        this.style.top = (pos[1] + deltaY) + 'px';
-        return false; // cancels scrolling
-      }
-    
-      function getCoors(e) {
-        var coors = [];
-        if (e.targetTouches && e.targetTouches.length) {
-          var thisTouch = e.targetTouches[0];
-          coors[0] = thisTouch.clientX;
-          coors[1] = thisTouch.clientY;
-        } else {
-          coors[0] = e.clientX;
-          coors[1] = e.clientY;
-        }
-        return coors;
-      }
+    var pos = [this.offsetLeft, this.offsetTop];
+    var that = this;
+    var origin = getCoors(e);
+
+    function moveDrag(e) {
+      var currentPos = getCoors(e);
+      var deltaX = currentPos[0] - origin[0];
+      var deltaY = currentPos[1] - origin[1];
+      this.style.left = (pos[0] + deltaX) + 'px';
+      this.style.top = (pos[1] + deltaY) + 'px';
+      return false; // cancels scrolling
     }
-    
-    var elements = document.querySelectorAll('.test-element');
-    [].forEach.call(elements, function (element) {
-      element.ontouchstart = element.onmspointerdown = startDrag;
-    });
-    
-    document.ongesturechange = function () {
-      return false;
+
+    function getCoors(e) {
+      var coors = [];
+      if (e.targetTouches && e.targetTouches.length) {
+        var thisTouch = e.targetTouches[0];
+        coors[0] = thisTouch.clientX;
+        coors[1] = thisTouch.clientY;
+      } else {
+        coors[0] = e.clientX;
+        coors[1] = e.clientY;
+      }
+      return coors;
     }
-  
-   
-    
+  }
+
+  var elements = document.querySelectorAll('.test-element');
+  [].forEach.call(elements, function (element) {
+    element.ontouchstart = element.onmspointerdown = startDrag;
+  });
+
+  document.ongesturechange = function () {
+    return false;
+  }
+
+
+
 });
 
