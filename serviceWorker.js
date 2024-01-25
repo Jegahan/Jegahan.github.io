740b0233cd0ceb63910b79c4b5a1bcdf622e1862
Jean-Gabriel Hanania <83063812+Jegahan@users.noreply.github.com>
2024-01-25 15:46:17 +0000

    service worker corrected

--- serviceWorker.js
+++ serviceWorker.js
@@ -16,18 +16,28 @@ const assets = [
 ];
 
 self.addEventListener("install", installEvent => {
-  console.log("Service worker installed with version" + version))
   installEvent.waitUntil(
     caches.open(staticDevCoffee).then(cache => {
       cache.addAll(assets);
+      let version = 3;
+      console.log("Service worker installed !");
+      console.log("version:" + version);
     })
   );
 });
 
+self.addEventListener("activate", function(e) {
+  let version = 3;
+  console.log("Service worker activated !");
+  console.log("version:" + version);
+}
+);
+
 self.addEventListener("fetch", fetchEvent => {
-  fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(res => {
-      return res || fetch(fetchEvent.request);
-    })
-  );
+fetchEvent.respondWith(
+  caches.match(fetchEvent.request).then(res => {
+    //console.log("Fetching service worker")
+    return res || fetch(fetchEvent.request);
+  })
+);
 });
