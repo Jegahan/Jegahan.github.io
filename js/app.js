9f49fdbdef1d0f4ea9adec89d8bb83b36d92deac
Jean-Gabriel Hanania <83063812+Jegahan@users.noreply.github.com>
2024-01-25 15:03:11 +0000

    } vergessen

--- js/app.js
+++ js/app.js
@@ -9,8 +9,10 @@ document.addEventListener('DOMContentLoaded', () => {
         console.error('Service Worker registration failed:', error);
       });
   }
+}
 
-  const container = document.querySelector(".container");
+  
+const container = document.querySelector(".container");
 
 
 //Save "Notes" object e.g. boxes objects inside elements
@@ -18,70 +20,70 @@ var elements = document.querySelectorAll('.test-element');
 
 
 //Add new notes ba appending new box object into boxContainer where all created notes land intially
-  function addBox() {
-    var boxContainer = document.getElementById("boxContainer");
-    var newBox = document.createElement("div");
-    newBox.className = "test-element";
-    newBox.textContent = "newTask"; // Add text or other content as needed
-    boxContainer.appendChild(newBox);
-    makeDraggable(newBox); // Make the new box draggable
-  }
+function addBox() {
+  var boxContainer = document.getElementById("boxContainer");
+  var newBox = document.createElement("div");
+  newBox.className = "test-element";
+  newBox.textContent = "newTask"; // Add text or other content as needed
+  boxContainer.appendChild(newBox);
+  makeDraggable(newBox); // Make the new box draggable
+}
 
 
 //Allows for moving notes
-  function makeDraggable(element) {
-    element.ontouchstart = element.onmspointerdown = startDrag;
+function makeDraggable(element) {
+  element.ontouchstart = element.onmspointerdown = startDrag;
+}
+
+
+var elements = document.querySelectorAll('.test-element');
+elements.forEach(makeDraggable);
+
+// Add new box on button click event. Is being called on pressing the add box button. (Box = Note)
+document.getElementById("addBoxBtn").addEventListener("click", addBox);
+
+
+function startDrag(e) {
+  this.ontouchmove = this.onmspointermove = moveDrag;
+
+  this.ontouchend = this.onmspointerup = function () {
+    this.ontouchmove = this.onmspointermove = null;
+    this.ontouchend = this.onmspointerup = null;
   }
 
+  var pos = [this.offsetLeft, this.offsetTop];
+  var that = this;
+  var origin = getCoors(e);
 
- var elements = document.querySelectorAll('.test-element');
- elements.forEach(makeDraggable);
+  function moveDrag(e) {
+    var currentPos = getCoors(e);
+    var deltaX = currentPos[0] - origin[0];
+    var deltaY = currentPos[1] - origin[1];
+    this.style.left = (pos[0] + deltaX) + 'px';
+    this.style.top = (pos[1] + deltaY) + 'px';
+    return false; // cancels scrolling
+  }
 
- // Add new box on button click event. Is being called on pressing the add box button. (Box = Note)
- document.getElementById("addBoxBtn").addEventListener("click", addBox);
+  function getCoors(e) {
+    var coors = [];
+    if (e.targetTouches && e.targetTouches.length) {
+      var thisTouch = e.targetTouches[0];
+      coors[0] = thisTouch.clientX;
+      coors[1] = thisTouch.clientY;
+    } else {
+      coors[0] = e.clientX;
+      coors[1] = e.clientY;
+    }
+    return coors;
+  }
+}
 
+var elements = document.querySelectorAll('.test-element');
+[].forEach.call(elements, function (element) {
+  element.ontouchstart = element.onmspointerdown = startDrag;
+});
 
-   function startDrag(e) {
-            this.ontouchmove = this.onmspointermove = moveDrag;
-
-            this.ontouchend = this.onmspointerup = function () {
-                this.ontouchmove = this.onmspointermove = null;
-                this.ontouchend = this.onmspointerup = null;
-            }
-
-            var pos = [this.offsetLeft, this.offsetTop];
-            var that = this;
-            var origin = getCoors(e);
-
-            function moveDrag(e) {
-                var currentPos = getCoors(e);
-                var deltaX = currentPos[0] - origin[0];
-                var deltaY = currentPos[1] - origin[1];
-                this.style.left = (pos[0] + deltaX) + 'px';
-                this.style.top = (pos[1] + deltaY) + 'px';
-                return false; // cancels scrolling
-            }
-
-            function getCoors(e) {
-                var coors = [];
-                if (e.targetTouches && e.targetTouches.length) {
-                    var thisTouch = e.targetTouches[0];
-                    coors[0] = thisTouch.clientX;
-                    coors[1] = thisTouch.clientY;
-                } else {
-                    coors[0] = e.clientX;
-                    coors[1] = e.clientY;
-                }
-                return coors;
-            }
-        }
-
-        var elements = document.querySelectorAll('.test-element');
-        [].forEach.call(elements, function (element) {
-            element.ontouchstart = element.onmspointerdown = startDrag;
-        });
-
-        document.ongesturechange = function () {
-            return false;
-        }
+document.ongesturechange = function () {
+  return false;
+}
 
